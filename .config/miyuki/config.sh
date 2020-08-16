a12048acc2b84244097af8d18e94c3d97ec05c91
Wenxin Wang <i@wenxinwang.me>
2020-08-16 15:09:52 +0800

    miyuki and paul

---
+++ .config/miyuki/config.sh
@@ -0,0 +1,5 @@
+declare -A WIKIS
+WIKIS["wiki"]="$HOME/04Personal/miyuki/wiki"
+WIKIS["lab"]="$HOME/02Projects/NetLab/LabWiki"
+WIKIS["kiwi"]="$HOME/04Personal/miyuki/kiwi"
+WIKIS["scratch"]="$HOME/zscratchpad"
---
+++ .config/paul/config.sh
@@ -0,0 +1,4 @@
+KEYWORDS=("TODAY" "TOMORROW" "THISWEEK" "NEXTWEEK" "THISMONTH" "NEXTMONTH" "SOMEDAY" "ARCHIVE")
+recvname="i"
+recvdomain="wenxinwang.me"
+sender="paul@wenxinwang.me"
