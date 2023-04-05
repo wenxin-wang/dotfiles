88e88a2a5e702d813bb2c1744af902ea591433e1
Wenxin Wang <i@wenxinwang.me>
2023-04-05 17:22:00 +0800

    guix: fix ca-cert

--- .profile.d/available/guix.sh
+++ .profile.d/available/guix.sh
@@ -3,3 +3,7 @@ export GUIX_PROFILE="$HOME/.guix-profile"
 . "$GUIX_PROFILE/etc/profile"
 
 export GUIX_LOCPATH="$HOME/.guix-profile/lib/locale"
+
+export SSL_CERT_DIR="$HOME/.guix-profile/etc/ssl/certs"
+export SSL_CERT_FILE="$HOME/.guix-profile/etc/ssl/certs/ca-certificates.crt"
+export GIT_SSL_CAINFO="$SSL_CERT_FILE"
