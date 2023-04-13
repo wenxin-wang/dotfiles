7ce5533aa43ab145f704110ee40fb18a7e0180b3
Wenxin Wang <i@wenxinwang.me>
2023-04-13 16:38:03 +0800

    emacs: update trusted themes

--- .config/emacs/etc/custom.el
+++ .config/emacs/etc/custom.el
@@ -4,10 +4,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("bfc0b9c3de0382e452a878a1fb4726e1302bf9da20e69d6ec1cd1d5d82f61e3d" default))
+   '("11873c4fbf465b956889adfa9182495db3bf214d9a70c0f858f07f6cc91cbd47" "bfc0b9c3de0382e452a878a1fb4726e1302bf9da20e69d6ec1cd1d5d82f61e3d" default))
  '(warning-suppress-log-types '((comp)))
- '(warning-suppress-types
-   '(((defvaralias losing-value woman-topic-history)))))
+ '(warning-suppress-types '(((defvaralias losing-value woman-topic-history)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
