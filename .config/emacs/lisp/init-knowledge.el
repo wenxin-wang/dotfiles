bee83d3bd9d06563b57de3b67ebb966a739dc289
Wenxin Wang <i@wenxinwang.me>
2023-04-18 22:59:24 +0800

    emacs: fix org src block indent

--- .config/emacs/lisp/init-knowledge.el
+++ .config/emacs/lisp/init-knowledge.el
@@ -11,11 +11,11 @@
   :config
   (require 'org-tempo)
   :custom
-  (;; Without this org mode will add indentation
-   ;; after headings.
-   ;; Also, use M-return for inserting headlines!
-   org-adapt-indentation nil
-   org-src-preserve-indentation t))
+  ;; Without this org mode will add indentation
+  ;; after headings.
+  ;; Also, use M-return for inserting headlines!
+  (org-adapt-indentation nil)
+  (org-src-preserve-indentation t))
 
 (use-package org-roam
   :defer t)
